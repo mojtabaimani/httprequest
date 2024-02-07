9d10bf11b9e2ee43344616b534045f56b203dcb9
Mojtaba Imani <sm.imani@gmail.com>
2024-02-08 00:38:25 +0100

    You provide token and it gets data

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfigurationRoot configuration = builder.Build();
+
+        var credentials = new
+        {
+            Username = configuration["Username"],
+            Password = configuration["Password"]
+        };
+        string authToken = configuration["AuthToken"];
+        string apiUrl = configuration["ApiUrl"];
+
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Add the X-Auth-Token header with your authentication token
+                client.DefaultRequestHeaders.Add("X-Auth-Token", authToken);
+
+                // Loop to run the request 10 times
+                for (int i = 0; i < 10; i++)
+                {
+                    // Send a GET request to the specified Uri
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    // Ensure we received a successful response
+                    response.EnsureSuccessStatusCode();
+
+                    // Read the response content as a string
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Print the response body and the iteration to the console
+                    Console.WriteLine($"Request #{i + 1} Response: {responseBody}");
+
+                    // Wait for 10 seconds before the next iteration
+                    Thread.Sleep(10000); // 10000 milliseconds = 10 seconds
+                }
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            // If something went wrong, print the error message to the console
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # httprequest
+
+This project is a HTTP request that allows you to send HTTP requests and handle responses.
+
+## Configuration
+
+Rename appsettings.json.sample to appsettings.json and add the value for ApiUrl and AuthToken.
+
+## Installation
+
+To install the library, you can use the following command:
+
+```c#
+dotnet restore
+```
+
+To start the program run:
+
+```c#
+dotnet run
+```
